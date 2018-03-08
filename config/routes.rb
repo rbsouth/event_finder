9373b8213ce1161584798a6969188b47f8ac181c
Reid South <rbsouth@gmail.com>
2018-03-08 14:27:46 -0700

    pulling

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   devise_for :businesses
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'user#index'
+  root 'users#index'
 end
