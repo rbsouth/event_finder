2de8b733d5b6b627c5812df26e04eea44c77a778
Reid South <rbsouth@gmail.com>
2018-03-08 16:44:01 -0700

    sign in and up pages styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'businesses/index'
+  get 'businesses/show'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
