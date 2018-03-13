25bb48176e16e14a25a4f1c6a417e9a40eb9f1d1
Reid South <rbsouth@gmail.com>
2018-03-13 14:48:39 -0600

    validations started

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,12 @@ class Event < ApplicationRecord
   has_many :users, through: :user_events
   has_many_attached :images
 
+  validates :title, presence: true
+	validates :address, presence: true, length: {minimum: 6}
+	validates :city, presence: true
+	validates :state, presence: true, length: {minimum: 2}
+	validates :start, presence: true, date: true
+
   # will either return this event's user_event or will return nil
   def user_event(current_user)
   	UserEvent.find_by(user: current_user, event_id: id)
